a172ed10ea638e9f1b5aad773634069ddbcd4bd4
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-03-07 06:57:18 +0100

    Good use of tsconfig.ts

--- dist/objetos/union-type.js
+++ dist/objetos/union-type.js
@@ -3,12 +3,10 @@
     let myCustomVariable = "Fernanod";
     console.log(typeof myCustomVariable);
     myCustomVariable = 20;
-    console.log(typeof myCustomVariable);
     myCustomVariable = {
         name: "Bruce",
         age: 43,
         powers: [1],
     };
-    console.log(myCustomVariable);
 })();
 //# sourceMappingURL=union-type.js.map
--- objetos/union-type.ts
+++ objetos/union-type.ts
@@ -10,12 +10,10 @@
   console.log(typeof myCustomVariable);
 
   myCustomVariable = 20;
-  console.log(typeof myCustomVariable);
 
   myCustomVariable = {
     name: "Bruce",
     age: 43,
     powers: [1],
   };
-  console.log(myCustomVariable);
 })();
