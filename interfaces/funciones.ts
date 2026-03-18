a285bfec9815e36448d3d266996c69169625e44a
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-03-18 06:49:15 +0100

    Interfaces

---
+++ interfaces/funciones.ts
@@ -0,0 +1,11 @@
+(() => {
+  interface addTwoNumbers {
+    (a: number, b: number): number;
+  }
+
+  let addNumberFunction: addTwoNumbers;
+
+  addNumberFunction = (a: number, b: number) => {
+    return 10;
+  };
+})();
